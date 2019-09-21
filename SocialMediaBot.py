b3643d10d43319c63bcf01e4f37e48f2735c65e6
Henry Creamer <bcards4me@verizon.net>
2019-09-21 11:14:30 -0500

    Added functions for transferring data from back end to front end so we can visualize the graphs

--- SocialMediaBot.py
+++
@@ -1,3 +0,0 @@
-"""
-A social media bot that will be used in the social media playground for researchers
-"""
---
+++ api/data_transfer.py
@@ -0,0 +1,35 @@
+import torch
+import json
+
+def initialize_matrix(matrix):
+    nodes = []
+    edges = []
+    n = matrix.shape
+    print(matrix.shape[0])
+    for i in range(matrix.shape[0]):
+        id_entry = {"id" : i}
+        nodes.append({"data" : id_entry})
+        for j in range(matrix.shape[1]):
+            if matrix[i][j] == 1:
+                edge_entry = {}
+                edge_entry["id"] = str(i) + str(j)
+                edge_entry["source"] = str(i)
+                edge_entry["target"] = str(j)
+                edges.append({"data" : edge_entry})
+    return json.dumps(nodes), json.dumps(edges)
+
+
+"""
+If the value is 1, then the color will be changed to red. If the value is -1, the color will be changed to blue
+"""
+def send_data_for_tick(matrix):
+    new_path = []
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if matrix[i][j] == 1:
+                key = str(i) + str(j)
+                new_path.append({key : "red"})
+            elif matrix[i][j] == -1:
+                key = str(i) + str(j)
+                new_path.append({key : "blue"})
+    return json.dumps(new_path)
